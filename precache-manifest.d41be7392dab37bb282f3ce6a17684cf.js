44b429ff4147cb02554a2482181f72f09f9b2811
1000111127 <shubhamshu37@gmail.com>
2023-04-18 07:37:32 +0530

    final

---
+++ precache-manifest.d41be7392dab37bb282f3ce6a17684cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "70f9e238a1e2e44426d5",
+    "url": "/static/css/main.9959cebf.chunk.css"
+  },
+  {
+    "revision": "70f9e238a1e2e44426d5",
+    "url": "/static/js/main.70f9e238.chunk.js"
+  },
+  {
+    "revision": "e365fac8f9a138c9a99b",
+    "url": "/static/js/1.e365fac8.chunk.js"
+  },
+  {
+    "revision": "de2d2858b5320f271c60",
+    "url": "/static/js/2.de2d2858.chunk.js"
+  },
+  {
+    "revision": "a83de6ac400fbe9a59cb",
+    "url": "/static/js/runtime~main.a83de6ac.js"
+  },
+  {
+    "revision": "5196306feaeb737b5bc08e05f95d7360",
+    "url": "/static/media/collectcoin.5196306f.mp3"
+  },
+  {
+    "revision": "71237d93461dc3d59ebbf49329e5f5cb",
+    "url": "/static/media/notification.71237d93.mp3"
+  },
+  {
+    "revision": "899bb990876717353f66bc07d64d2786",
+    "url": "/static/media/tadaa.899bb990.mp3"
+  },
+  {
+    "revision": "2633a00f719bdb411136b47159b7bfa9",
+    "url": "/index.html"
+  }
+];
